bf2d7a52ab6e2635eaf1852436b1566e584f50b8
ZubrSV <ZubrSV@gmail.com>
2023-07-09 16:32:40 +0300

    Change in init class

---
+++ src/main/java/common/CommonAction.java
@@ -0,0 +1,32 @@
+package common;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.util.TimeUtils;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import static common.Config.PLATFORM_AND_BROWSER;
+import static constants.Constants.TimeVariables.IMPLICIT_WAIT;
+
+public class CommonAction {
+
+    public WebDriver createDriver(){
+        WebDriver driver = null;
+
+        switch (PLATFORM_AND_BROWSER){
+            case "win_chrome":
+               System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        }
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(IMPLICIT_WAIT));
+        return driver;
+    }
+
+    public void waitForPageLoadComplete(Duration timeToWait, WebDriver driver) {
+        new WebDriverWait(driver, timeToWait).until(webDriver -> ((JavascriptExecutor) webDriver)
+                .executeScript("return document.readyState").equals("complete"));
+    }
+}
---
+++ src/main/java/constants/Constants.java
@@ -0,0 +1,8 @@
+package constants;
+
+public class Constants {
+
+    public static class TimeVariables {
+        public static final int IMPLICIT_WAIT = 4;
+    }
+}
